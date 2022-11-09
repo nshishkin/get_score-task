22e7c3c9e8e9a65cc19ac1dad4d32547e6cfc689
Nikolay <spwawnick@gmail.com>
2022-11-09 12:20:57 +0300

    added task solution, using filter function

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from pprint import pprint
 import random
 import math
 
-TIMESTAMPS_COUNT = 50000
+TIMESTAMPS_COUNT = 500
 
-PROBABILITY_SCORE_CHANGED = 0.0001
+PROBABILITY_SCORE_CHANGED = 0.1
 
 PROBABILITY_HOME_SCORE = 0.45
 
@@ -22,7 +22,7 @@ INITIAL_STAMP = {
 def generate_stamp(previous_value):
     score_changed = random.random() > 1 - PROBABILITY_SCORE_CHANGED
     home_score_change = 1 if score_changed and random.random() > 1 - \
-        PROBABILITY_HOME_SCORE else 0
+                             PROBABILITY_HOME_SCORE else 0
     away_score_change = 1 if score_changed and not home_score_change else 0
     offset_change = math.floor(random.random() * OFFSET_MAX_STEP) + 1
 
@@ -51,8 +51,26 @@ pprint(game_stamps)
 
 
 def get_score(game_stamps, offset):
-    '''
-        Takes list of game's stamps and time offset for which returns the scores for the home and away teams.
-        Please pay attention to that for some offsets the game_stamps list may not contain scores.
-    '''
+    # median_offset_increase = (OFFSET_MAX_STEP + 1) / 2
+    #
+    # search_pointer = math.floor(offset / median_offset_increase)
+    # print(search_pointer)
+    # if game_stamps[search_pointer]["offset"] == offset:
+    #     return (game_stamps[search_pointer]["score"]["home"],
+    #             game_stamps[search_pointer]["score"]["away"])
+    # else:
+    #     return game_stamps[search_pointer]
+
+    arr = list(filter(lambda item: (abs(item['offset']-offset) <= OFFSET_MAX_STEP), game_stamps))
+    print(arr)
+    offset_index = 0
+    offset_delta = offset - arr[0]['offset']
+    for index, item in enumerate(arr):
+        if abs(item['offset']-offset) < offset_delta:
+            offset_delta = abs(item['offset']-offset)
+            offset_index = index
+    return arr[offset_index]
     # return home, away
+
+
+print(get_score(game_stamps, 990))
